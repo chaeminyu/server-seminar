baa9721c4f936d8927c727298b83c92e3b715aed
Chaemin Yu <chaemin.u@gmail.com>
2025-07-05 21:48:58 +0900

    feat: project create

---
+++ myproject/wsgi.py
@@ -0,0 +1,17 @@
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+
+application = get_wsgi_application()
+
+# ===========================
+# myproject/asgi.py
+# ===========================
+
+import os
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+
+application = get_asgi_application()
