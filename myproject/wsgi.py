2b5283b14b14392edf172195026241dddd6644bc
Chaemin Yu <chaemin.u@gmail.com>
2025-07-05 23:04:05 +0900

    fix: asgi

--- myproject/wsgi.py
+++ myproject/wsgi.py
@@ -4,14 +4,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
 application = get_wsgi_application()
-
-# ===========================
-# myproject/asgi.py
-# ===========================
-
-import os
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
-
-application = get_asgi_application()
